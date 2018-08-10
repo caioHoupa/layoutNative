2d0c3009841db29a56eb9e74e9d14e8f8206b0d0
Caio <caio@houpa.app>
2018-08-10 16:49:34 -0300

    layoout cad OK

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -10,7 +10,7 @@ export class HomeComponent implements OnInit {
 
     constructor(page:Page) {
         // hidden action bar
-        page.actionBarHidden = true;
+        // page.actionBarHidden = true;
     }
     ngOnInit(): void {
     }
